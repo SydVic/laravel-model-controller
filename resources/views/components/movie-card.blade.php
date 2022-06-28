116498b583ff4a5cd3308897e5255bbfa0f0e388
SydVic <fabrizio.bocca27@gmail.com>
2022-06-28 15:47:27 +0200

    aggiunto poster url in database e stile card

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,10 +1,27 @@
+{{-- MOVIE CARD --}}
 <div class="movie-card">
-  <h3>Title: {{ $movie->title }}</h3>
-  <h5>Original Title: {{ $movie->original_title }}</h5>
-  <p>Nationality: {{ $movie->nationality }}</p>
-  <p>Release Date: {{ $movie->date }}</p>
-  <h4>Vote: {{ $movie->vote }}</h4>
-  {{-- @php
-      dd($movie);
-  @endphp --}}
+  {{-- CARD INNER --}}
+  <div class="card-inner">
+    {{-- CARD FRONT --}}
+    <div class="card-front">
+      <div class="poster-wrapper">
+        <img src="{{ $movie->poster }}" alt="{{ $movie->original_title }}">
+      </div>
+    </div>
+    {{-- /CARD FRONT --}}
+
+    {{-- CARD BACK --}}
+    <div class="card-back">
+      <div class="text">
+        <h3>Title: {{ $movie->title }}</h3>
+        <h5>Original Title: {{ $movie->original_title }}</h5>
+        <p>Nationality: {{ $movie->nationality }}</p>
+        <p>Release Date: {{ $movie->date }}</p>
+        <h4>Vote: {{ $movie->vote }}</h4>
+      </div>
+    </div>
+    {{-- /CARD BACK --}}
+  </div>
+  {{-- /CARD INNER --}}
 </div>
+{{-- /MOVIE CARD --}}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,12 @@
 
 @section('main_content')
 
-  @foreach ($movies as $movie)
-    @include('components.movie-card', $movie)
-  @endforeach
+  <div class="container">
+    <div class="movies-container">
+      @foreach ($movies as $movie)
+        @include('components.movie-card', $movie)
+      @endforeach
+    </div>
+  </div>
 
 @endsection
