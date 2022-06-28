744de819fae268228f409568c012c0829bf059da
SydVic <fabrizio.bocca27@gmail.com>
2022-06-28 14:46:46 +0200

    aggiunta stampa dati dinamica

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,5 +1,9 @@
 <div class="movie-card">
-  <h2>{{ $movie->title }}</h2>
+  <h3>Title: {{ $movie->title }}</h3>
+  <h5>Original Title: {{ $movie->original_title }}</h5>
+  <p>Nationality: {{ $movie->nationality }}</p>
+  <p>Release Date: {{ $movie->date }}</p>
+  <h4>Vote: {{ $movie->vote }}</h4>
   {{-- @php
       dd($movie);
   @endphp --}}
